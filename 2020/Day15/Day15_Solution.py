7c7ed62c1c5e97ab3c1dc81b13e187127f1c1cf0
simon <simon.bergsten@hotmail.com>
2020-12-20 20:50:23 +0100

    added day 15 b

--- 2020/Day15/Day15_Solution.py
+++ 2020/Day15/Day15_Solution.py
@@ -35,3 +35,31 @@ def solve_a():
 
 
 print(solve_a())
+
+
+def solve_b():
+    inp = getData()
+    n = 0
+    nums = {}
+    last = 0
+    for i in range(len(inp)):
+        n += 1
+        nums[inp[i]] = [n]
+        last = inp[i]
+
+    while n != 30000000:
+        n += 1
+        if len(nums[last]) == 1:
+            new = 0
+        else:
+            new = nums[last][-1] - nums[last][-2]
+        if new in nums:
+            nums[new].append(n)
+        else:
+            nums[new] = [n]
+
+        last = new
+
+    return last
+
+print(solve_b())
